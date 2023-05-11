2660c64009e2c77a0de4866b9b2835ae05b33b0b
Aditiya Taruna <aditiyatarun@gmail.com>
2023-05-11 09:19:20 +0700

    Upload file

---
+++ app/Http/Controllers/Routebostrap.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class routebostrap extends Controller
+{
+    public function bostrap1() {
+        return view("praktikum1.index")  ;
+        }
+}
